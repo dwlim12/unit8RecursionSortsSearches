b1387567a115ecb137d34ceeae927ded0d0c1e0f
dwlim12 <DWLim@student.naperville203.org>
2016-03-16 10:29:14 -0500

    Tree Lab

--- FractalTreeLab/TreePanel.java
+++ FractalTreeLab/TreePanel.java
@@ -9,87 +9,58 @@ import javax.swing.JPanel;
 
 public class TreePanel extends JPanel
 {
-   private final int PANEL_WIDTH = 400;
-   private final int PANEL_HEIGHT = 400;
+    private final int PANEL_WIDTH = 400;
+    private final int PANEL_HEIGHT = 500;
+    private final double EPSILON = 10E-4;
+    private final double SQ = Math.sqrt(3.0) / 6;
 
-   private final double SQ = Math.sqrt(3.0) / 6;
+    private final int XONE = 200, YONE = 500;
+    private final int XTWO = 200, YTWO = 400;
+    private final double INITLENGTH = 100;
+    private final double INITANGLE = (Math.PI / 3);
 
-   private final int TOPX = 200, TOPY = 20;
-   private final int LEFTX = 60, LEFTY = 300;
-   private final int RIGHTX = 340, RIGHTY = 300;
+    //-----------------------------------------------------------------
+    //  Sets the initial fractal order to the value specified.
+    //-----------------------------------------------------------------
+    public TreePanel ()
+    {
+        setBackground (Color.WHITE);
+        setPreferredSize (new Dimension(PANEL_WIDTH, PANEL_HEIGHT));
+    }
 
-   private int current; //current order
+    //-----------------------------------------------------------------
+    //  Draws the fractal recursively. Base case is an order of 1 for
+    //  which a simple straight line is drawn. Otherwise three
+    //  intermediate points are computed, and each line segment is
+    //  drawn as a fractal.
+    //-----------------------------------------------------------------
+//     public void drawFractal (double x1, double y1, double length, double angle, Graphics2D g2)
+//     {
+//         double deltaX, deltaY, x2, y2, angle2;
+//         //make terminating condition branch length
+//         if (length <= EPSILON)
+//             return;
+//         else
+//         {
+//             g2.drawLine(x1,y1,XTWO,YTWO);
+//             deltaX = length * Math.sin(angle);
+//             deltaY = length * Math.cos(angle);
+//             //          drawFractal (x1, y1,,, page);
+//             //          drawFractal (x2, y2,,, page);
+//             //          drawFractal (x3, y3,,, page);
+//             //          drawFractal (x4, y4,,, page);
+//         }
+//     }
 
-   //-----------------------------------------------------------------
-   //  Sets the initial fractal order to the value specified.
-   //-----------------------------------------------------------------
-   public TreePanel ()
-   {
-      setBackground (Color.black);
-      setPreferredSize (new Dimension(PANEL_WIDTH, PANEL_HEIGHT));
-   }
+    //-----------------------------------------------------------------
+    //  Performs the initial calls to the drawFractal method.
+    //-----------------------------------------------------------------
+    public void paintComponent (Graphics2D g2)
+    {
+        super.paintComponent (g2);
 
-   //-----------------------------------------------------------------
-   //  Draws the fractal recursively. Base case is an order of 1 for
-   //  which a simple straight line is drawn. Otherwise three
-   //  intermediate points are computed, and each line segment is
-   //  drawn as a fractal.
-   //-----------------------------------------------------------------
-   public void drawFractal (int order, int x1, int y1, int x5, int y5,
-                            Graphics page)
-   {
-      int deltaX, deltaY, x2, y2, x3, y3, x4, y4;
+        g2.setColor (Color.BLACK);
 
-      if (order == 1)
-         page.drawLine (x1, y1, x5, y5);
-      else
-      {
-         deltaX = x5 - x1;  // distance between end points
-         deltaY = y5 - y1;
-
-         x2 = x1 + deltaX / 3;  // one third
-         y2 = y1 + deltaY / 3;
-
-         x3 = (int) ((x1+x5)/2 + SQ * (y1-y5));  // tip of projection
-         y3 = (int) ((y1+y5)/2 + SQ * (x5-x1));
-
-         x4 = x1 + deltaX * 2/3;  // two thirds
-         y4 = y1 + deltaY * 2/3;
-
-         drawFractal (order-1, x1, y1, x2, y2, page);
-         drawFractal (order-1, x2, y2, x3, y3, page);
-         drawFractal (order-1, x3, y3, x4, y4, page);
-         drawFractal (order-1, x4, y4, x5, y5, page);
-      }
-   }
-
-   //-----------------------------------------------------------------
-   //  Performs the initial calls to the drawFractal method.
-   //-----------------------------------------------------------------
-   public void paintComponent (Graphics page)
-   {
-      super.paintComponent (page);
-
-      page.setColor (Color.green);
-
-      drawFractal (current, TOPX, TOPY, LEFTX, LEFTY, page);
-      drawFractal (current, LEFTX, LEFTY, RIGHTX, RIGHTY, page);
-      drawFractal (current, RIGHTX, RIGHTY, TOPX, TOPY, page);
-   }
-
-   //-----------------------------------------------------------------
-   //  Sets the fractal order to the value specified.
-   //-----------------------------------------------------------------
-   public void setOrder (int order)
-   {
-      current = order;
-   }
-
-   //-----------------------------------------------------------------
-   //  Returns the current order.
-   //-----------------------------------------------------------------
-   public int getOrder ()
-   {
-      return current;
-   }
+        g2.drawLine (XONE, YONE, XTWO, YTWO);
+    }
 }
--- FractalTreeLab/TreeViewer.java
+++ FractalTreeLab/TreeViewer.java
@@ -6,20 +6,16 @@
 //********************************************************************
 
 import java.awt.*;
-import java.awt.event.*;
 import javax.swing.*;
 
-public class TreeViewer implements ActionListener
+public class TreeViewer extends JFrame
 {
     private final int WIDTH = 400;
     private final int HEIGHT = 500;
 
-    private final int MIN = 1, MAX = 9;
-
-    private JButton increase, decrease;
-    private JLabel titleLabel, orderLabel;
+    private JLabel titleLabel;
     private TreePanel drawing;
-    private JPanel panel, tools;
+    private JPanel panel;
     private JFrame frame;
 
     //-----------------------------------------------------------------
@@ -32,65 +28,15 @@ public class TreeViewer implements ActionListener
 
     public TreeViewer()
     {
-        tools = new JPanel ();
-        tools.setLayout (new BoxLayout(tools, BoxLayout.X_AXIS));
-        tools.setBackground (Color.yellow);
-        tools.setOpaque (true);
-
-        titleLabel = new JLabel ("Fractal Tree");
-        titleLabel.setForeground (Color.black);
-
-        increase = new JButton (new ImageIcon ("increase.gif"));
-        increase.setPressedIcon (new ImageIcon ("increasePressed.gif"));
-        increase.setMargin (new Insets (0, 0, 0, 0));
-        increase.addActionListener (this);
-        decrease = new JButton (new ImageIcon ("decrease.gif"));
-        decrease.setPressedIcon (new ImageIcon ("decreasePressed.gif"));
-        decrease.setMargin (new Insets (0, 0, 0, 0));
-        decrease.addActionListener (this);
-
-        orderLabel = new JLabel ("Order: 1");
-        orderLabel.setForeground (Color.black);
-
-        tools.add (titleLabel);
-        tools.add (Box.createHorizontalStrut (20));
-        tools.add (decrease);
-        tools.add (increase);
-        tools.add (Box.createHorizontalStrut (20));
-        tools.add (orderLabel);
-
-        drawing = new TreePanel (1);
-
+        drawing = new TreePanel();
+        
         panel = new JPanel();
-        panel.add (tools);
         panel.add (drawing);
 
-        frame = new JFrame();
-        frame.setTitle("Knoch Snowflake");
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setSize(WIDTH, HEIGHT);
-        frame.add(panel);
-        frame.setVisible(true);
-    }
-
-    //-----------------------------------------------------------------
-    //  Determines which button was pushed, and sets the new order
-    //  if it is in range.
-    //-----------------------------------------------------------------
-    public void actionPerformed (ActionEvent event)
-    {
-        int order = drawing.getOrder();
-
-        if (event.getSource() == increase)
-            order++;
-        else
-            order--;
-
-        if (order >= MIN && order <= MAX)
-        {
-            orderLabel.setText ("Order: " + order);
-            drawing.setOrder (order);
-            frame.repaint();
-        }
+        this.setTitle("Knoch Snowflake");
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setSize(WIDTH, HEIGHT);
+        this.add(panel);
+        this.setVisible(true);
     }
 }
